3831d91855cf8a4940fef3dff7a62f333ad8fc95
Joshua Riek <jjriek@verizon.net>
2017-08-27 03:46:45 -0400

    dir updates

--- libc/build.bat
+++ libc/build.bat
@@ -1,20 +1,20 @@
 @echo off
-SET PATH=Tools\VC152\;Tools\;Tools\qemu\
+SET PATH=Tools\VC152\;..\Tools\;Tools\qemu\
 
 :build_library
     rem Compile all source assembly files
-    ML /omf /c /Fl /Sa /Zi /I include src\*.asm
+    ML /omf /c /Fl /Zi /I include src\*.asm
     rem ML /omf /c /I include src\*.asm
 
     rem Link all object files to libary
     LIB lib\libc -+conio.obj -+ctype.obj -+stdio.obj -+string.obj -+assert.obj -+bios.obj -+signal.obj -+time.obj, lib\libc.lst, lib\libc.lib
 
-    move /Y *.obj src\obj > nul
-    move /Y *.lst src\lst > nul
+    move /Y *.obj src\obj
+    move /Y *.lst src\lst
 
 :build_tests
     rem Compile all C files
-    CL /AT /G2 /Gs /Gx /c /Fl /Zi /Zl /Sa /I include tests\*.c
+    CL /AT /G2 /Gs /Gx /c /Fl /Zi /Zl /I include tests\*.c
     rem CL /AT /G2 /Gs /Gx /c /I include tests\*.c
 
     rem Compile all assembly files
@@ -24,8 +24,8 @@ SET PATH=Tools\VC152\;Tools\;Tools\qemu\
     rem Link together all files and include the libary
     LINK /T /NOD  kernel.obj __kernel.obj __time.obj __signal.obj __stddef.obj __string.obj __ctype.obj __conio.obj, bin\kernel.bin, nul, lib\libc.lib, nul
 
-    move /Y *.obj tests\obj > nul
-    move /Y *.cod tests\cod > nul
+    move /Y *.obj tests\obj
+    move /Y *.cod tests\cod
     del  *.pdb > nul
 
 :build_floppy
@@ -42,6 +42,6 @@ SET PATH=Tools\VC152\;Tools\;Tools\qemu\
 
     rem Write the bootloader to the floppy disk
     dd if=bin\bootload.bin of=bin\built_floppy.img bs=512
-
+pause
     rem Run the built floppy disk image with qemu
     start bochsrc.bxrc
